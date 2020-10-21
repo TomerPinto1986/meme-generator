31cb1e59df1ca970c82ac96384b6bd0da175d37a
Tomer Pinto <t.pintoosi@gmail.com>
2020-10-21 13:51:24 +0300

    first Phase

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,3 +1,39 @@
 'use strict';
 
 console.log('hello controller!');
+var gCanvas;
+var gCtx;
+
+function onInit() {
+    gCanvas = document.querySelector('#my-canvas');
+    gCtx = gCanvas.getContext('2d');
+    console.log('The context:', gCtx);
+    openMemeEditor();
+}
+
+
+function openMemeEditor(imgId = 1) {
+    const imgUrl = getImgUrlFromService(imgId);
+    drawImg(imgUrl);
+}
+
+function drawText() {
+    const lines = getLinesFromService();
+    console.log(lines);
+    gCtx.fillStyle = `${lines[0].color}`
+    gCtx.font = `${lines[0].size}px ${lines[0].font}`
+    gCtx.textAlign = `${lines[0].align}`
+    gCtx.fillText(lines[0].txt, 100, 50)
+    gCtx.strokeText(lines[0].txt, 100, 50)
+}
+
+
+
+function drawImg(imgUrl) {
+    const img = new Image()
+    img.src = imgUrl;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        drawText();
+    }
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,3 +1,46 @@
 'use strict';
 
 console.log('hello service!');
+
+var gNextIdx = 101;
+const gImgs = [
+    { id: 1, url: 'img/1.jpg' },
+    { id: 2, url: 'img/2.jpg' },
+    { id: 3, url: 'img/3.jpg' },
+    { id: 4, url: 'img/4.jpg' },
+    { id: 5, url: 'img/5.jpg' },
+    { id: 6, url: 'img/6.jpg' },
+]
+const gMeme = createMeme();
+
+
+function getImgUrlFromService(id) {
+    const currImg = gImgs.find(img => img.id === id);
+    return currImg.url;
+}
+
+
+function getLinesFromService(idx = 101) {
+    const lines = [];
+    gMeme.lines.map(line => lines.unshift(line));
+    return lines;
+}
+
+
+
+
+
+function createMeme(selectedImgId = 1) {
+    return {
+        idx: gNextIdx++,
+        selectedImgId,
+        selectedLineIdx: 0,
+        lines: [{
+            txt: 'Start The Fun',
+            size: 40,
+            font: 'Impact',
+            align: 'left',
+            color: 'red',
+        }]
+    }
+}
